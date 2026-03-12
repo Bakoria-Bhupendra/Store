cec31da355f1b2809d8be716bafd8cd9169dda62
Bakoria-Bhupendra <bakoriabhupendra@gmail.com>
2026-03-12 20:34:49 +0530

    Store Service

---
+++ src/main/java/com/bobby/store/PaypalPaymentService.java
@@ -0,0 +1,12 @@
+package com.bobby.store;
+
+import org.springframework.stereotype.Service;
+
+@Service("Paypal")
+public class PaypalPaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PAYPAL PAYMENT");
+        System.out.println("amount: " + amount);
+    }
+}
---
+++ src/main/java/com/bobby/store/StripPaymentService.java
@@ -0,0 +1,34 @@
+package com.bobby.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("Strip")
+@Primary
+public class StripPaymentService implements PaymentService {
+    @Value("${stripe.apiURL}")
+    private String apiURL;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> currencies;
+
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIP PAYMENT");
+        System.out.println("API URL: " + apiURL);
+        System.out.println("Enabled: " + enabled);
+        System.out.println("Timeout: " + timeout);
+        System.out.println("Currencies: " + currencies);
+        System.out.println("amount: " + amount);
+    }
+}
