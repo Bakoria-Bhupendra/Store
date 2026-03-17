91e05a156dcbdc64461626197e255a0613fd063c
Bakoria-Bhupendra <bakoriabhupendra@gmail.com>
2026-03-17 16:06:12 +0530

    success database integreat

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -1,24 +1,21 @@
-/* Creating the schema */
-create schema store;
-
-/*Using the database store */
-use store;
-
-/*creating the table and data inside the database*/
-create table users (
-                       id bigint not null auto_increment primary key,
-                       name varchar (255) not null,
-                       email varchar (255) not null,
-                       password varchar (255) not null
+create table users
+(
+    id       bigint auto_increment
+        primary key,
+    name     varchar(255) not null,
+    email    varchar(255) not null,
+    password varchar(255) not null
 );
 
-create table addresses (
-                           id bigint not null auto_increment,
-                           street varchar (255) not null,
-                           city varchar (255) not null,
-                           zip varchar (255) not null,
-                           user_id bigint not null,
-                           constraint fk_userid
-                               foreign key (user_id)
-                                   references users(id)
+create table addresses
+(
+    add_id  bigint auto_increment
+        primary key,
+    street  varchar(255) not null,
+    city    varchar(255) not null,
+    zip     varchar(255) not null,
+    user_id bigint       not null,
+    constraint addresses__fk
+        foreign key (user_id) references users (id)
 );
+
